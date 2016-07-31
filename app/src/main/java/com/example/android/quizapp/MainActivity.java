cdb489a512a8bc1b76d9275462c0534ce166ea1b
KatiaBravo <katia@alexbravo.com>
2016-07-31 16:32:32 -0700

    fix grade

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -19,51 +19,56 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public double answerForQ1() {
-        CheckBox checkBox = (CheckBox) findViewById(R.id.other_check_box);
-        CheckBox checkBox2 = (CheckBox) findViewById(R.id.basketball_check_box);
-        CheckBox checkBox3 = (CheckBox) findViewById(R.id.tennis_check_box);
-        if (!checkBox.isChecked() && checkBox2.isChecked() && checkBox3.isChecked()) {
-            overallScore = overallScore + 1;
-        } else if(checkBox2.isChecked() || checkBox3.isChecked()){
-            overallScore = overallScore + 0.5;
+
+    public void answerForQ1() {
+        CheckBox checkBoxO = (CheckBox) findViewById(R.id.other_check_box);
+        CheckBox checkBoxB = (CheckBox) findViewById(R.id.basketball_check_box);
+        CheckBox checkBoxT = (CheckBox) findViewById(R.id.tennis_check_box);
+        if (!checkBoxO.isChecked() && checkBoxB.isChecked() && checkBoxT.isChecked()) {
+            overallScore++;
         }
-        return overallScore;
     }
-    public double answerForQ2() {
+
+    public void answerForQ2() {
         RadioButton radioButton = (RadioButton) findViewById(R.id.saira_kaylee_radio_button);
-        if (radioButton.isSelected()) {
-            overallScore = overallScore + 1;
+        if (radioButton.isChecked()) {
+            overallScore++;
         }
-        return overallScore;
     }
-    public double answerForQ3() {
+
+    public void answerForQ3() {
         EditText editText = (EditText) findViewById(R.id.names_field_edit_text);
-        if (editText.getText().toString().equalsIgnoreCase("Ayaka, Irina")) {
-            overallScore = overallScore + 1;
+        if (editText.getText().toString().equalsIgnoreCase("Ayaka and Irina")) {
+            overallScore++;
+        } else {
+            overallScore += 1;
         }
-        return overallScore;
     }
-    public double answerForQ4() {
+
+    public void answerForQ4() {
         CheckBox checkBox = (CheckBox) findViewById(R.id.chinese_check_box);
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.pinyin_check_box);
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.other_two_check_box);
         if (checkBox.isChecked() && checkBox2.isChecked() && !checkBox3.isChecked()) {
-            overallScore = overallScore + 1;
+            overallScore++;
         }
-        return overallScore;
     }
-    public double answerForQ5() {
+
+    public void answerForQ5() {
         RadioButton radioButton = (RadioButton) findViewById(R.id.five_radio_button);
-        if (radioButton.isSelected()) {
-            overallScore = overallScore + 1;
+        if (radioButton.isChecked()) {
+            overallScore++;
         }
-        return overallScore;
     }
+
     public void submitTest(View view) {
-        double score = answerForQ1() + answerForQ2() + answerForQ3() + answerForQ4() + answerForQ5();
-        String toastMessage = "You got " + score + "/7.0";
         overallScore = 0.0;
+        answerForQ1();
+        answerForQ2();
+        answerForQ3();
+        answerForQ4();
+        answerForQ5();
+        String toastMessage = "You got " + overallScore + "/5.0";
         Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
