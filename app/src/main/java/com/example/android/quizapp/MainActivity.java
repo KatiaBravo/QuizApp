4ff8c08f3bf7d807dc7e89b313c56511c9d028b7
KatiaBravo <katia@alexbravo.com>
2016-07-31 14:42:20 -0700

    Have bug fixes and is way better

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -21,22 +21,26 @@ public class MainActivity extends AppCompatActivity {
     }
     public double answerForQ1() {
         CheckBox checkBox = (CheckBox) findViewById(R.id.other_check_box);
-        if (checkBox.isChecked()) {
-            overallScore += 1;
+        CheckBox checkBox2 = (CheckBox) findViewById(R.id.basketball_check_box);
+        CheckBox checkBox3 = (CheckBox) findViewById(R.id.tennis_check_box);
+        if (!checkBox.isChecked() && checkBox2.isChecked() && checkBox3.isChecked()) {
+            overallScore = overallScore + 1;
+        } else if(checkBox2.isChecked() || checkBox3.isChecked()){
+            overallScore = overallScore + 0.5;
         }
         return overallScore;
     }
     public double answerForQ2() {
         RadioButton radioButton = (RadioButton) findViewById(R.id.saira_kaylee_radio_button);
         if (radioButton.isSelected()) {
-            overallScore += 1;
+            overallScore = overallScore + 1;
         }
         return overallScore;
     }
     public double answerForQ3() {
         EditText editText = (EditText) findViewById(R.id.names_field_edit_text);
-        if (editText.getText().toString().equals("Ayaka, Irina")) {
-            overallScore += 1;
+        if (editText.getText().toString().equalsIgnoreCase("Ayaka, Irina")) {
+            overallScore = overallScore + 1;
         }
         return overallScore;
     }
@@ -44,24 +48,22 @@ public class MainActivity extends AppCompatActivity {
         CheckBox checkBox = (CheckBox) findViewById(R.id.chinese_check_box);
         CheckBox checkBox2 = (CheckBox) findViewById(R.id.pinyin_check_box);
         CheckBox checkBox3 = (CheckBox) findViewById(R.id.other_two_check_box);
-        if (checkBox.isChecked()) {
-            overallScore += 1;
-        }
-        if(checkBox2.isChecked()) {
-            overallScore += 1;
+        if (checkBox.isChecked() && checkBox2.isChecked() && !checkBox3.isChecked()) {
+            overallScore = overallScore + 1;
         }
         return overallScore;
     }
     public double answerForQ5() {
         RadioButton radioButton = (RadioButton) findViewById(R.id.five_radio_button);
         if (radioButton.isSelected()) {
-            overallScore += 1;
+            overallScore = overallScore + 1;
         }
         return overallScore;
     }
     public void submitTest(View view) {
-        double score = (answerForQ1() + answerForQ2() + answerForQ3() + answerForQ4() + answerForQ5())/2;
-        String toastMessage = "You got " + score + "/6.0";
+        double score = answerForQ1() + answerForQ2() + answerForQ3() + answerForQ4() + answerForQ5();
+        String toastMessage = "You got " + score + "/7.0";
+        overallScore = 0.0;
         Toast.makeText(this, toastMessage, Toast.LENGTH_SHORT).show();
     }
 }
